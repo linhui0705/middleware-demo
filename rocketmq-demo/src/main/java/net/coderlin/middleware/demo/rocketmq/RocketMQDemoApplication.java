8f6fdd319401bd13ea7673bc950f9a473ba1f181
linhui <linhui0705@gmail.com>
2021-08-22 14:40:09 +0800

    refactor: rename application

--- rocketmq-demo/src/main/java/net/coderlin/middleware/demo/rocketmq/RocketMQDemoApplication.java
+++ rocketmq-demo/src/main/java/net/coderlin/middleware/demo/rocketmq/RocketMQDemoApplication.java
@@ -4,15 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Title: Application
+ * Title: RocketMQDemoApplication
  * Description:
  *
  * @author Lin Hui
  * Created on 2021/8/21 12:00:03
  */
 @SpringBootApplication
-public class Application {
+public class RocketMQDemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(RocketMQDemoApplication.class, args);
     }
 }
